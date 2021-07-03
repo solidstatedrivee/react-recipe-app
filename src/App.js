688b5d521bc198b1b1eabee62e25e1337124b66c
Nate Sims <nsims@ggc.edu>
2021-07-03 15:01:05 -0400

    init project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,142 @@
-import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
+import { useState } from 'react';
+import React from 'react';
+import Modal from 'react-modal';
+import RecipeCard from './RecipeCard';
+
+const customStyles = {
+  overlay: {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(244, 197, 174, 0.75)'
+  },
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    background: 'rgb(91,91,91)',
+    border: '1px solid rgb(56,56,56)',
+    padding: '50px 25px',
+
+  }
+}
+
+Modal.setAppElement('#root');
 
 function App() {
+  let subtitle;
+
+  const [query, setQuery] = useState("");
+  const [menuClosed, setMenuClosed] = useState(true);
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+  const [recipes, setrecipes] = useState([]);
+  const [healthLabel, sethealthLabel] = useState("keto-friendly");
+
+
+  let YOUR_APP_ID = '54c0cda2';
+  let YOUR_APP_KEY = '0a336849942061bad4c6a8766ccba5f3';
+
+
+  let url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}
+  &health=${healthLabel}`;
+
+
+  function toggleMenuState() {
+    setMenuClosed(!menuClosed);
+    setIsOpen(!modalIsOpen);
+
+    menuClosed ? document.body.classList.add("freeze-view") : document.body.classList.remove("freeze-view");
+  }
+
+  function afterOpenModal() {
+    subtitle.style.color = 'rgb(242, 158, 112)';
+  }
+
+  async function getRecipes() {
+    let getErrorMessage = document.getElementsByClassName('error-message')[0];
+    let showError = () => getErrorMessage.classList.add('show-error');
+    let hideError = () => getErrorMessage.classList.remove('show-error');
+
+    try {
+      let result = await Axios.get(url);
+      if (result.data.hits.length < 1 || result.data.q === "") {
+        showError();
+      } else {
+        hideError();
+        setrecipes(result.data.hits);
+        console.log(result.data);
+      }
+    } catch (err) {
+      showError();
+    }
+
+  }
+
+  let onSubmit = (e) => {
+    e.preventDefault();
+    getRecipes();
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="menu-icon-container" onClick={toggleMenuState}>
+        <i className={menuClosed ? 'fad fa-bars menu-icon' : 'fad fa-times menu-icon'}></i>
+      </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        style={customStyles}
+        contentLabel='Recipe App Menu'
+      >
+        <div className="modal-content-wrapper">
+          <h2 ref={(_subtitle) => (subtitle = _subtitle)} className='menu-title'>Menu</h2>
+          <div className="menu-social-links">
+            <ul className='social-links'>
+              <li className='social-link'><i class="fab fa-instagram"></i>Instagram</li>
+              <li className='social-link'><i class="fab fa-facebook-f"></i>Facebook</li>
+              <li className='social-link'><i class="fab fa-twitter"></i>Twitter</li>
+              <li className='social-link'><i class="fab fa-tiktok"></i>TikTok</li>
+            </ul>
+          </div>
+          <button className='close-modal' onClick={toggleMenuState}><i class="far fa-times"></i>Close</button>
+          <p>Made by Nate Sims | 2021</p>
+
+        </div>
+
+      </Modal>
+      <h1 className="app-title">Let us help you find a recipe! 🍳</h1>
+      <form action="" className="app-search-form" onSubmit={onSubmit}>
+        <div className="error-message">Sorry, we couldn't find what you were looking for 😬</div>
+        <input type="text" placeholder='Raspberry Pie 🥧' className='app-search' value={query} onChange={(e) => setQuery(e.target.value)} />
+        <div className="dietary-restrictions-container">
+          <label htmlFor="health-labels" className="health-label-for">Dietary Restrictions?: </label>
+
+          <select name="health-labels" id="health-labels" onChange={e => sethealthLabel(e.target.value)}>
+            <option className='health-label' >keto-friendly</option>
+            <option className='health-label' >dairy-free</option>
+            <option className='health-label' >gluten-free</option>
+            <option className='health-label' >pork-free</option>
+            <option className='health-label' >vegan</option>
+          </select>
+        </div>
+
+        <input type="submit" value="Find Recipes" className="app-find" />
+      </form>
+
+      <div className='recipe-cards-container'>
+        {recipes.map(recipe => {
+          return <RecipeCard recipe={recipe} />
+        })}
+      </div>
+
     </div>
   );
 }
---
+++ src/RecipeCard.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+
+import "./RecipeCard.css"
+
+let fileExtensionRegex = /^.*\.(jpe?g|JPE?G|png)$/
+
+
+function RecipeCard({ recipe }) {
+    const [show, setshow] = useState(true);
+
+    let toggleShow = () => {
+        setshow(!show);
+    }
+    return (
+        recipe["recipe"]["image"].match(fileExtensionRegex) != null &&
+        (<div className="recipe-ingredients-container">
+            <div className='recipe-card' style={{ background: `url(${recipe["recipe"]["image"]}) center` }}>
+                <h2 className='recipe-label'>{recipe["recipe"]["label"]}</h2>
+                {recipe["recipe"]["cuisineType"] != null && (
+                    <div className="cuisine-type-container">
+                        <i class="fad fa-utensils-alt"></i>
+                        <div className="cuisine-type">Cuisine: {recipe["recipe"]["cuisineType"]}</div>
+                    </div>
+                )}
+                <div className="calories">{Math.floor(recipe["recipe"]["calories"])}cal</div>
+                <div className="show-container" onClick={toggleShow}>
+                    <i class={show ? "fad fa-angle-double-down" : "fad fa-minus"}></i>
+                    <div className="show">{show ? 'Show More' : 'Show Less'}</div>
+                </div>
+            </div>
+            <div className={`ingredients-container ${!show ? 'ingredients-visible' : ''}`}>
+                <h3>Ingredients</h3>
+                <ul>
+                    {recipe["recipe"]["ingredients"].map(ingredient => {
+                        return (
+                            <li>• {ingredient["text"]}</li>)
+                    })}
+
+                </ul>
+                <a href={recipe["recipe"]["url"]} target="_blank" className="recipe-link"><i class="fad fa-link"></i>View Recipe</a>
+            </div>
+        </div>
+        ))
+}
+
+export default RecipeCard
